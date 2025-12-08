f1d931351565a874f4d3bef4cf1103cd14a3a197
Ubuntu <ubuntu@ip-172-31-11-66.eu-west-1.compute.internal>
2025-12-08 06:44:00 +0000

    Fixed the issue

--- file_test.py
+++ file_test.py
@@ -1,17 +1,21 @@
 import pytest
 
+
 def test_cal_addition():
-	output = 2 + 4
-	assert output == 6
+    output = 2 + 4
+    assert output == 6
+
 
 def test_cal_subtraction():
-	output = 2 -4
-	assert output == -2
+    output = 2 - 4
+    assert output == -2
+
 
 def test_cal_multiplication():
-	output = 2 * 4
-	assert output == 8
+    output = 2 * 4
+    assert output == 8
+
 
 def test_coucou():
-	output = "Hello"
-	assert output == "Hello" 
+    output = "Hello"
+    assert output == "Hello" 
