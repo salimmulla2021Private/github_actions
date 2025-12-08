5ded61a5cc44670b0cc3fac78ed76c1bcf193e29
Ubuntu <ubuntu@ip-172-31-11-66.eu-west-1.compute.internal>
2025-12-08 06:26:52 +0000

    Added flake8 testt

---
+++ file_test.py
@@ -0,0 +1,17 @@
+import pytest
+
+def test_cal_addition():
+	output = 2 + 4
+	assert output == 6
+
+def test_cal_subtraction():
+	output = 2 -4
+	assert output == -2
+
+def test_cal_multiplication:
+	output = 2 * 4
+	assert output == 8
+
+def test_coucou():
+	output = "Hello"
+	assert output == "Hello" 
